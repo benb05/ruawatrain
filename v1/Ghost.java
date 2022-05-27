c0883aab0cc4a155080f86e383b41059126cba6e
Josiah-Moltz <jmoltz30@stuy.edu>
2022-05-27 12:34:35 -0400

    v1

---
+++ v1/Ghost.java
@@ -0,0 +1,24 @@
+// Ruawatrain :: Benjamin Belotser, David Deng, Josiah Moltz
+// APCS pd6
+// FP01
+
+public class Ghost {
+  private char[][] _maze;
+  private int gX, gY; // ghost's position
+  private boolean _solved;
+
+  // ACCESSORS
+  public int getGX() {
+    return gX;
+  }
+
+  public int getGY() {
+    return gY;
+  }
+
+  // MUTATORS
+
+  // SOLVER
+
+  // MOVER
+}
